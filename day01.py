d7f782a21a24ae70aba9b11eb67fd244352ec09d
Craig Robson <craig@zhatt.com>
2018-12-07 13:11:31 -0800

    Day 1: Chronal Calibration

---
+++ day01.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+
+import aoc
+
+def part1( input ):
+    """
+    Calculate the final frequency based on a list of changes.
+    """
+
+    frequency = 0
+
+    for change in input:
+        frequency += int( change )
+
+    return frequency
+
+
+def part2( input ):
+    """
+    Find the first frequency seen first when processing changes.  You may need
+    to process the input list multiple times.
+    """
+
+    # Read the input into a list.
+    changes = list()
+    for change in input:
+        changes.append( int( change ) )
+
+    frequency = 0
+
+    seen = set()
+    seen.add( 0 )
+
+    while True:
+        for change in changes:
+            frequency += change
+
+            if frequency in seen:
+                return frequency
+
+            seen.add( frequency )
+
+
+if __name__ == "__main__":
+    args = aoc.parse_args()
+
+    with open( args.inputfile ) as input:
+
+        if args.part == 1:
+            output = part1( input )
+            print( output )
+        else:
+            output = part2( input )
+            print( output )
---
+++ test_day01.py
@@ -0,0 +1,55 @@
+import unittest
+
+import day01
+
+class TestDay01( unittest.TestCase ):
+
+    example = ( 1, -2, 3, 1 )
+
+    def test_part1_example1( self ):
+        result = day01.part1( self.example )
+        self.assertEqual( result, 3 )
+
+    def test_part1_example2( self ):
+        result = day01.part1( ( 1, 1, 1 ) )
+        self.assertEqual( result, 3 )
+
+    def test_part1_example3( self ):
+        result = day01.part1( ( 1, 1, -2 ) )
+        self.assertEqual( result, 0 )
+
+    def test_part1_example4( self ):
+        result = day01.part1( ( -1, -2, -3 ) )
+        self.assertEqual( result, -6 )
+
+    def test_part1_input( self ):
+        result = day01.part1( open( 'day01.input' ) )
+        self.assertEqual( result, 561 )
+
+
+    def test_part2_example1( self ):
+        result = day01.part2( self.example )
+        self.assertEqual( result, 2 )
+
+    def test_part2_example2( self ):
+        result = day01.part2( ( 1, -1 ) )
+        self.assertEqual( result, 0 )
+
+    def test_part2_example3( self ):
+        result = day01.part2( ( 3, 3, 4, -2, -4 ) )
+        self.assertEqual( result, 10 )
+
+    def test_part2_example4( self ):
+        result = day01.part2( ( -6, 3, 8, 5, -6 ) )
+        self.assertEqual( result, 5 )
+
+    def test_part2_example5( self ):
+        result = day01.part2( ( 7, 7, -2, -7, -4 ) )
+        self.assertEqual( result, 14 )
+
+    def test_part2_input( self ):
+        result = day01.part2( open( 'day01.input' ) )
+        self.assertEqual( result, 563 )
+
+if __name__ == '__main__':
+    unittest.main()
