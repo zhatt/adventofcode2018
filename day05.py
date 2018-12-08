a9d9bf5c60dc163e19836cb2b521092c7779013e
Craig Robson <craig@zhatt.com>
2018-12-08 15:57:18 -0800

    Day 5: Alchemical Reduction

---
+++ day05.py
@@ -0,0 +1,64 @@
+#!/usr/bin/env python3
+
+import aoc
+import string
+
+def reactPolymer( polymer ):
+
+    # Scan polymer and copy everything but reactive pairs to nextPolymer for
+    # next interation.  Keep iterating until no reactive pairs are found.
+    done = False
+    while not done:
+        done = True
+        nextPolymer = ''
+
+        i = 0
+        while i <= len( polymer ) - 2:
+            if polymer[i] != polymer[i+1] and polymer[i].lower() == polymer[i+1].lower():
+                # Drop reactive pair.
+                i += 2
+                done = False
+            else:
+                # Copy character that is not part of reactive pair.
+                nextPolymer += polymer[i]
+                i += 1
+
+        # The last character was not destroyed so copy it.
+        if i < len( polymer ):
+            nextPolymer += polymer[i]
+
+        polymer = nextPolymer
+
+    return polymer
+
+def part1( input ):
+    """
+    Find size of reacted polymer.
+    """
+    polymer = input[ 0 ]
+
+    reactedPolymer = reactPolymer( polymer )
+
+    return len( reactedPolymer )
+
+
+def part2( input ):
+    """
+    Find smalled reacted polymer obtained when removing each character.
+    """
+    polymer = input[ 0 ]
+
+    shortest = len( polymer )
+
+    # Remove each character and perform reaction to find which removed character
+    # yields the shorts polymer.
+    for c in string.ascii_uppercase:
+        polymerToTest = polymer.translate( None, c + c.lower() )
+        reactedPolymer = reactPolymer( polymerToTest )
+        shortest = min( shortest, len( reactedPolymer ) )
+
+    return shortest
+
+if __name__ == "__main__":
+    aoc.main( part1, part2 )
+
---
+++ test_day05.py
@@ -0,0 +1,48 @@
+
+import unittest
+
+import aoc
+import day05
+
+class TestDay05( unittest.TestCase ):
+
+    example = [ 'dabAcCaCBAcCcaDA' ]
+
+    def test_part1_example1( self ):
+        result = day05.part1( self.example )
+        self.assertEqual( result, 10 )
+
+    def test_part1_1( self ):
+        result = day05.part1( [ 'Aa' ] )
+        self.assertEqual( result, 0 )
+
+    def test_part1_2( self ):
+        result = day05.part1( [ 'AaXXAa' ] )
+        self.assertEqual( result, 2 )
+
+    def test_part1_3( self ):
+        result = day05.part1( [ 'AaAaXXAa' ] )
+        self.assertEqual( result, 2 )
+
+    def test_part1_4( self ):
+        result = day05.part1( [ 'XXXAaAaXXAa' ] )
+        self.assertEqual( result, 5 )
+
+    def test_part1_4( self ):
+        result = day05.part1( [ 'XXXAaAaXX' ] )
+        self.assertEqual( result, 5 )
+
+    def test_part1_input( self ):
+        result = day05.part1( aoc.readInput( 'day05.input' ) )
+        self.assertEqual( result, 11152 )
+
+    def test_part2_example1( self ):
+        result = day05.part2( self.example )
+        self.assertEqual( result, 4 )
+
+    def test_part2_input( self ):
+        result = day05.part2( aoc.readInput( 'day05.input' ) )
+        self.assertEqual( result, 6136 )
+
+if __name__ == '__main__':
+    unittest.main()
